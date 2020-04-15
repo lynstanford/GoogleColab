bb15bfda33a8f3a81d333c43caac6303bc858d0d
Lyn Stanford <lynstanford@users.noreply.github.com>
2020-04-15 01:09:43 -0400

    Add files via upload
    Python files uploaded include basic projects for Google Colabs.

---
+++ seattle_temperature.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import snaps
+
+temp = snaps.get_weather_temp(latitude=47.61, longitude=122.33)
+print('The temperature in Seattle is:', temp)
+
+
+   
+   
+   
+    
